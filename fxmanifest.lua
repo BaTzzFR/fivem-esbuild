75c847e3f3b29380878190c5c350fa3c7b2f00ff
BaTzzFR <leodemol07@gmail.com>
2023-05-07 10:45:56 +0200

    commit: fixed infinite loop and added worker farm

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'common'
 
 author 'BaTzzFR'
 description 'Builds resources with esbuild. To learn more: https://esbuild.github.io'
-version '1.0.1'
+version '1.0.2'
 
 server_script 'esbuild_builder.js'
 
